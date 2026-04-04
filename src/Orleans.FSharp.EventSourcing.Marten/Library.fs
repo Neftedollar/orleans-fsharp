cf34bfe2021dfd48e0638f6680754c9ba001c320
Roman Melnikov <roman@neftedollar.com>
2026-04-04 21:28:31 +0300

    refactor: extract Marten into optional Orleans.FSharp.EventSourcing.Marten plugin
    Move MartenConfig from Orleans.FSharp.EventSourcing to the new
    Orleans.FSharp.EventSourcing.Marten package so Marten is an optional
    dependency. Orleans.FSharp.EventSourcing no longer pulls in Marten.

---
+++ src/Orleans.FSharp.EventSourcing.Marten/Library.fs
@@ -0,0 +1,8 @@
+namespace Orleans.FSharp.EventSourcing.Marten
+
+/// <summary>
+/// Marker type used to identify the Orleans.FSharp.EventSourcing.Marten assembly for reflection.
+/// </summary>
+type AssemblyMarker =
+    class
+    end
